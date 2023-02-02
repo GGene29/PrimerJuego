6b275d63914ec4168936f121f4e9c6467af279c0
Love <sumozacalcurian@gmail.com>
2023-02-02 17:05:05 -0400

    Agregando imagenes

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 function love.load()
-
+    fondo = love.graphics.newImage("practicas/assets/fondo.png")
 end
 
-function love.update()
+function love.update(dt)
 
 end
 
 function love.draw()
+    love.graphics.draw(fondo)
     love.graphics.print("Hola Mundo" , love.mouse.getX(), love.mouse.getY() )
+    
 end
